dce48a91d56eecadf096b621c8af8396bbe531c5
Ax <105896053+Awxel@users.noreply.github.com>
2022-05-20 12:18:44 +0700

    First upload

---
+++ database.js
@@ -0,0 +1,84 @@
+let makanan =  {
+
+    produk_detail : {
+        kode: "BRG001",
+        nama:  "Jus",
+        stock: 10,
+        rating: 5.0,
+        harga: 15000,
+        gambar:"https://asset.kompas.com/crops/Hbr1HWQH187bZoPFV39dBArEPvg=/108x34:1000x628/750x500/data/photo/2021/08/18/611caefa8a063.jpg"
+    },
+    produk_detail2 : {
+        nama:"Bakso",
+        kode: "BRG002",
+        stock: 20,
+        rating: 5.0,
+        harga: 15000,
+        gambar:"https://cdns.klimg.com/merdeka.com/i/w/news/2021/08/27/1345870/content_images/670x335/20210827142321-1-ilustrasi-bakso-009-tantri-setyorini.jpg"
+    },
+    produk_detail3 : {
+        nama: "Heci",
+        kode: "BRG003",
+        stock: 100,
+        rating: 5.0,
+        harga: 2000,
+        gambar:"http://assets.kompasiana.com/items/album/2020/07/30/whatsapp-image-2020-07-26-at-15-09-13-5f227332d541df2648175cc2.jpeg"
+    },
+    produk_detail4 : {
+        nama: "Permen",
+        kode: "BRG004",
+        stock: 1000,
+        rating: 5.0,
+        harga: 500,
+        gambar:"https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/intisarifoto/original/36530_kisah-permen-yang-manis.png"
+    },
+    produk_detail5 : {
+        nama: "Air Mineral",
+        kode: "BRG005",
+        stock: 30,
+        rating: 5.0,
+        harga: 6000,
+        gambar:"https://res.cloudinary.com/dk0z4ums3/image/upload/v1589962541/attached_image/cara-memilih-air-kemasan-yang-baik.jpg"
+    },
+    produk_detail6 : {
+        nama: "Pangsit Mie Ayam",
+        kode: "BRG006",
+        stock: 40,
+        rating: 5.0,
+        harga: 12000,
+        gambar:"https://cdns.klimg.com/diadona.id/resized/640x320/news/2020/03/21/7007/8-resep-cara-membuat-mie-pangsit-ayam-dengan-kuah-sederhana-yang-enak-khas-jakarta-2003212.jpg"
+    },
+    produk_detail7 : {
+        nama: "Ayam Geprek",
+        kode: "BRG007",
+        stock: 50,
+        rating: 5.0,
+        harga: 20000,
+        gambar:"https://kbu-cdn.com/dk/wp-content/uploads/kulit-ayam-crispy-geprek.jpg"
+    },
+    produk_detail8 : {
+        nama: "Sushi",
+        kode: "BRG008",
+        stock: 80,
+        rating: 5.0,
+        harga: 35000,
+        gambar:"https://static.honestdocs.id/system/blog_articles/main_hero_images/000/002/990/original/Ikan_Mentah_Pada_Hidangan_Sushi__Aman_Untuk_di_Konsumsi.jpg"
+    },
+    produk_detail9 : {
+        nama: "Nasi Goreng",
+        kode: "BRG009",
+        stock: 30,
+        rating: 5.0,
+        harga: 12000,
+        gambar:"http://kbu-cdn.com/dk/wp-content/uploads/nasi-goreng-setan.jpg"
+    },
+    produk_detail10 : {
+        nama: "Tempe Penyet",
+        kode: "BRG0010",
+        stock: 25,
+        rating: 5.0,
+        harga: 13000,
+        gambar:"https://selerasa.com/wp-content/uploads/2021/08/Resep-tempe-penyet-1-1200x798.jpg"
+    },
+};
+    console.log (makanan)
